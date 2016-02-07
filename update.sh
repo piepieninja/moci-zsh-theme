566f9d538f6735f0fe0b145c1769356c587efd3e
piepieninja <piepieninja@gmail.com>
2016-02-06 20:59:57 -0500

    stable prompt

--- update.sh
+++ update.sh
@@ -1,18 +1,11 @@
-#!/bin/bash
-
 echo 'Updating theme...'
-
 if [ ! -f ~/.oh-my-zsh/themes/moci.zsh-theme ]; then
     cp moci.zsh-theme ~/.oh-my-zsh/themes/
 else
     rm ~/.oh-my-zsh/themes/moci.zsh-theme
     cp moci.zsh-theme ~/.oh-my-zsh/themes/
 fi
-
-
 echo 'DONE!'
 echo 'Reloading...'
-
 . ~/.zshrc
-
 echo 'DONE!'
